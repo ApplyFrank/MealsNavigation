184df89c13131ad8cf4e96631ac58ec69c148272
Frank Su <frank.su@applydigital.co>
2024-05-19 13:14:24 -0700

    Pass itemData into individual flatlist

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,24 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
-function MealsOverviewScreen() {
+function MealsOverviewScreen({ route }) {
+  // just like useNavigation we can use useRoute() to get access to route.params
+  const catId = route.params.categoryId;
+
+  const displayMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  // Automatically received it from renderItem
+  function renderMealItem(itemdata) {
+    console.log(JSON.stringify(itemdata));
+    return <MealItem title={itemdata.item.title} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <FlatList data={displayMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
     </View>
   );
 }
