0511ca3dd95c3006b13c648eabf7ba6aad0dfd12
Frank Su <frank.su@applydigital.co>
2024-05-19 23:09:28 -0700

    Set styling to navigation with options and screenOptions

--- App.js
+++ App.js
@@ -15,8 +15,23 @@ export default function App() {
       <StatusBar style="dark" />
       <NavigationContainer>
         {/**FRANK NOTE The first child will be the default screen. But you can use initialRouteName */}
-        <Stack.Navigator initialRouteName="MealsCategories">
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        {/** Apply the styles to all the screens by setting screenOptions in Stack.Navigator */}
+        <Stack.Navigator
+          initialRouteName="MealsCategories"
+          screenOptions={{
+            headerStyle: { backgroundColor: '#351401' },
+            headerTintColor: 'white',
+            contentStyle: { backgroundColor: '#3f2f25' },
+          }}
+        >
+          {/** However each screens options would override */}
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: 'All categories',
+            }}
+          />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
         </Stack.Navigator>
       </NavigationContainer>
